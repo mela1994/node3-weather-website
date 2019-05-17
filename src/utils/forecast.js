07211dbf8e27970dd23b8640726ed424d68d75c3
Nikola199407 <48557222+Nikola199407@users.noreply.github.com>
2019-05-17 10:02:16 +0200

    Added new information i forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. Max temperature for today is :'+body.daily.data[0].apparentTemperatureMax+' and min temerature is :'+body.daily.data[0].apparentTemperatureMin+' There is a ' + body.currently.precipProbability + '% chance of rain.')
+        
         }
     })
 }
